63017024e6130857836dfb65ae80f636846b6abe
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2018-06-04 08:51:18 +0530

    Add template and render it

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var site = require('./script.js');
 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/static'));
+
 app.use('/site', site);
 
 app.listen(8080);
--- script.js
+++ script.js
@@ -2,11 +2,23 @@ var express = require('express');
 var router = express.Router()
 
 router.use('/home', function(req, res) {
-	res.send("Homepage");
+	res.render('pages/index');
 });
 
-router.use('/something', function(req, res) {
-	res.send("somthing");
+router.use('/experience', function(req, res) {
+	res.render('pages/experience');
+});
+
+router.use('/education', function(req, res) {
+	res.render('pages/education');
+});
+
+router.use('/skills', function(req, res) {
+	res.render('pages/skills');
+});
+
+router.use('/interests', function(req, res) {
+	res.render('pages/interests');
 });
 
 module.exports = router
